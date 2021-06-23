5f57daddb02ddee4a8cc1a4e35fce8d9d60efb32
Leoforpeng <leo.peng@shtycon.com>
2021-06-23 17:06:01 +0800

    修改文件名

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ReadCard msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ReadCard from './components/ReadCard.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ReadCard
   }
 }
 </script>
