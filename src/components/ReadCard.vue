a157d1686cfe9668fe7397c3435a6aac21def0b2
Leoforpeng <leo.peng@shtycon.com>
2021-06-24 15:21:11 +0800

    实现跳转google浏览器

--- src/components/ReadCard.vue
+++ src/components/ReadCard.vue
@@ -123,6 +123,7 @@
       </form>
     </div>
     <div>
+      <button @click="changeCharm()">切换谷歌浏览器</button>
       <div>
         <video id="video" width="500px" height="500px" autoplay="autoplay"></video>
         <canvas id="canvas" width="500px" height="500px"></canvas>
@@ -276,19 +277,18 @@ export default {
       }
     },
 
+    changeCharm(){
+      // eslint-disable-next-line no-undef
+      var objShell= new ActiveXObject("WScript.Shell");
+      objShell.Run("cmd.exe /c start chrome  http://127.0.0.1:8080/",0,true);
+    },
+
     getMedia(){
       let video = document.getElementById("video");
       let constraints = {
         video: {width: 500, height: 500},
         audio: true
       };
-      /*
-      这里介绍新的方法:H5新媒体接口 navigator.mediaDevices.getUserMedia()
-      这个方法会提示用户是否允许媒体输入,(媒体输入主要包括相机,视频采集设备,屏幕共享服务,麦克风,A/D转换器等)
-      返回的是一个Promise对象。
-      如果用户同意使用权限,则会将 MediaStream对象作为resolve()的参数传给then()
-      如果用户拒绝使用权限,或者请求的媒体资源不可用,则会将 PermissionDeniedError作为reject()的参数传给catch()
-      */
       let promise = navigator.mediaDevices.getUserMedia(constraints);
       promise.then(function (MediaStream) {
         video.srcObject = MediaStream;
