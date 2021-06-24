7349c719aeb604884fc9ae6822e61504b2627878
Leoforpeng <leo.peng@shtycon.com>
2021-06-24 13:44:56 +0800

    拍摄，不支持IE

--- src/components/ReadCard.vue
+++ src/components/ReadCard.vue
@@ -118,16 +118,24 @@
         <textarea rows="6" cols="100" name="base64Image" ></textarea><br />
         <input id="button1" type="button" value="读取" name="btnRead" @click="readCard()" />
         <input id="button2" type="reset" value="重置" name="btnReset" />
-        <input id="button4" type="button" value="提交数据库" name="btnDB" @click="SetDB()" />
-        <input id="button5" type="button" value="保存数据" name="btnData" @click="SetData()" />
+        <input id="button3" type="button" value="保存数据" name="btnData" @click="SetData()" />
+        <input id="button4" type="button" value="认证核验" name="btnCheckFace" @click="CheckFace()" />
       </form>
+    </div>
+    <div>
+      <div>
+        <video id="video" width="500px" height="500px" autoplay="autoplay"></video>
+        <canvas id="canvas" width="500px" height="500px"></canvas>
+      </div>
+      <input type="button" title="开启摄像头" value="开启摄像头" @click="getMedia()" />
+      <button id="snap" @click="takePhoto()">拍照</button>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'ReadCard',
   data() {
     return {
       form: {
@@ -267,8 +275,40 @@ export default {
         form1.base64Image.value ="";
       }
     },
+
+    getMedia(){
+      let video = document.getElementById("video");
+      let constraints = {
+        video: {width: 500, height: 500},
+        audio: true
+      };
+      /*
+      这里介绍新的方法:H5新媒体接口 navigator.mediaDevices.getUserMedia()
+      这个方法会提示用户是否允许媒体输入,(媒体输入主要包括相机,视频采集设备,屏幕共享服务,麦克风,A/D转换器等)
+      返回的是一个Promise对象。
+      如果用户同意使用权限,则会将 MediaStream对象作为resolve()的参数传给then()
+      如果用户拒绝使用权限,或者请求的媒体资源不可用,则会将 PermissionDeniedError作为reject()的参数传给catch()
+      */
+      let promise = navigator.mediaDevices.getUserMedia(constraints);
+      promise.then(function (MediaStream) {
+        video.srcObject = MediaStream;
+        video.play();
+      }).catch(function (PermissionDeniedError) {
+        console.log(PermissionDeniedError);
+      })
+    },
+    takePhoto() {
+      let video = document.getElementById("video");
+      //获得Canvas对象
+      let canvas = document.getElementById("canvas");
+      let ctx = canvas.getContext('2d');
+      ctx.drawImage(video, 0, 0, 500, 500);
+    },
     SetData() {
-      console.log('dadadada')
+      console.log('上传数据')
+    },
+    CheckFace(){
+      console.log('人证核验')
     }
   }
 }
