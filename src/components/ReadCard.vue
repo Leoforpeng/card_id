88e4e3c33bdcdd1a362fbcf95e16447abb05e182
Leoforpeng <leo.peng@shtycon.com>
2021-09-08 14:53:17 +0800

    修改为ctrl + M 2

--- src/components/ReadCard.vue
+++ src/components/ReadCard.vue
@@ -12,9 +12,9 @@
         <param name="_Version" value="65536"/>
         <param name="_ExtentX" value="2646"/>
         <param name="_ExtentY" value="1323"/>
-        <param name="_StockProps" value="0"/>
+        <param name="_StockProps" value="0"/>M
       </object>
-      <input id="button1" type="button" value="读取" name="btnRead" @click="readCard()" title="Ctrl + N 可快速读卡"/>
+      <input id="button1" type="button" value="读取" name="btnRead" @click="readCard()" title="Ctrl + M 可快速读卡"/>
     </div>
   </div>
 </template>
